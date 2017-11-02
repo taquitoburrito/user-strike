b40fc052f92ca6423e55291a9b8d542340c5ee54
taquitoburrito <joemccarthy67@gmail.com>
2017-11-02 11:13:49 -0500

    fixed inputs

--- py/start.py
+++ py/start.py
@@ -3,13 +3,13 @@ import os
 print('Welcome to User Strike \n This is an information intelligence program to track and classify individuals \n You are responsible for any information you discover using this program. \n If you have any questions or suggestions please email me at tburrito@usa.com')
 ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
 
-if ans == 1:
+if ans == '1':
     UserTarget = input("What is the target?(User URL) \n")
     yn = input('Is this correct(y/n)? \n' + UserTarget + '\n')
-elif ans == 2:
+elif ans == '2':
     createArray = input('How many users are you going to search for?')
-elif ans == 3:
+elif ans == '3':
     open()
-elif ans == 4:
+elif ans == '4':
     print('Goodbye')
-    quit
+    
