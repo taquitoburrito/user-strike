b71148a59360d9b889e5414384ec24cf88bbcbfb
taquitoburrito <joemccarthy67@gmail.com>
2017-11-04 16:07:43 -0500

    Adding options

--- py/start.py
+++ py/start.py
@@ -4,6 +4,20 @@ print('Welcome to User Strike \n This is an information intelligence program to 
 ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
 
 if ans == '1':
+    Knowyn = input("Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)")
+    if Knowyn == 'y':
+        FirstName = input("What is the Targets First Name? (If unknown just hit 'Enter')")
+        if FirstName == '':
+            LastName = input("What is the Targets Last Name? (If unknown just hit 'Enter')")
+        else:
+            LastName = input("What is " + FirstName + "'s Last name?")
+        CorrectName = input('Is ' + FirstName + ' ' + LastName + ' Correct? Y/N')
+        if CorrectName == 'y' or 'Y' or 'yes' or 'YES':
+
+        else:
+            ans = input('Would you like to restart? Y/N')
+            if ans == 'y' or 'Y' or 'yes' or 'YES':
+                ######## Add module to restart here ########
     UserTarget = input("What is the target?(User URL) \n")
     yn = input('Is this correct(y/n)? \n' + UserTarget + '\n')
 elif ans == '2':
