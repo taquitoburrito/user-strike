e464934cc41cdf4ca6dd82ff0c32826e26cd6fd8
taquitoburrito <joemccarthy67@gmail.com>
2017-11-08 18:07:32 -0600

    Changing from if/thens to cases

--- py/start.py
+++ py/start.py
@@ -3,10 +3,12 @@ import os
 print('Welcome to User Strike \n This is an information intelligence program to track and classify individuals \n You are responsible for any information you discover using this program. \n If you have any questions or suggestions please email me at tburrito@usa.com')
 ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
 
-if ans == '1':
-    Knowyn = input("Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)")
-    if Knowyn == 'y':
-        FirstName = input("What is the Targets First Name? (If unknown just hit 'Enter')")
+def anscase(ans):
+    case 1:
+        Knowyn = input("Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)")
+        if Knowyn == 'y':
+            FirstName = input("What is the Targets First Name? (If unknown just hit 'Enter')")
+        else:
         if FirstName == '':
             LastName = input("What is the Targets Last Name? (If unknown just hit 'Enter')")
         else:
