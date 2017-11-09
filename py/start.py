2c7463fd5bf12364c01a4651fa988a0f16bf37d3
taquitoburrito <joemccarthy67@gmail.com>
2017-11-09 12:17:56 -0600

    Splitting into modules
    Might just start from scratch before this gets more complex

--- py/start.py
+++ py/start.py
@@ -3,7 +3,20 @@ import os
 print('Welcome to User Strike \n This is an information intelligence program to track and classify individuals \n You are responsible for any information you discover using this program. \n If you have any questions or suggestions please email me at tburrito@usa.com')
 ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
 
+
 def anscase(ans):
+    
+                ######## Add module to restart here ########
+    UserTarget = input("What is the target?(User URL) \n")
+    yn = input('Is this correct(y/n)? \n' + UserTarget + '\n')
+elif ans == '2':
+    createArray = input('How many users are you going to search for?')
+elif ans == '3':
+    open()
+elif ans == '4':
+    input('Goodbye')
+    
+def knowledge(ans):
     case 1:
         Knowyn = input("Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)")
         if Knowyn == 'y':
@@ -19,14 +32,4 @@ if CorrectName == 'y' or 'Y' or 'yes' or 'YES':
             else:
                 ans = input('Would you like to restart? Y/N')
                 if ans == 'y' or 'Y' or 'yes' or 'YES':
-                ######## Add module to restart here ########
-    UserTarget = input("What is the target?(User URL) \n")
-    yn = input('Is this correct(y/n)? \n' + UserTarget + '\n')
-elif ans == '2':
-    createArray = input('How many users are you going to search for?')
-elif ans == '3':
-    open()
-elif ans == '4':
-    input('Goodbye')
     
-def StrData(Knowyn, FirstName, LastName, Usertarget, createArray):
