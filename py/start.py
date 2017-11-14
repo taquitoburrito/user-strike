05263a443ae5931e2e19c6920fed7b581caf5e7a
taquitoburrito <joemccarthy67@gmail.com>
2017-11-13 19:57:12 -0600

    Few steps closer to working

--- py/start.py
+++ py/start.py
@@ -1,7 +1,4 @@
 import os
-print('Welcome to User Strike \n This is an information intelligence program to track and classify individuals \n You are responsible for any information you discover using this program. \n If you have any questions or suggestions please email me at tburrito@usa.com')
-ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
-anscase(ans)
 def anscase(ans):
     if ans == '1':
         singleU()
@@ -35,23 +32,28 @@ def foundProfiles():
 
 def inquireIfKnown():
     #find out any additional information about targets
-    knownYN.lower() = input('Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)')
+    knownYN = input('Do you already know some information about the target? Y/N (Selecting Yes will inquire about their name, alieses, known profiles, etc.)')
+    knownYN = knownYN.lower()
     if knownYN == 'y':
         FirstName = input("What is the Targets First Name? (If unknown just hit 'Enter')")
-    else:
-        break
     if FirstName == '':
         LastName = input('What is the Targets Last Name? (If unknown just hit "enter")')
     else:
         LastName = input("What is " + FirstName + "'s Last name?")
-    CorrectName.lower() = input('Is ' + FirstName + ' ' + LastName + ' Correct? Y/N')
+    CorrectName = input('Is ' + FirstName + ' ' + LastName + ' Correct? Y/N')
+    CorrectName = CorrectName.lower()
     if CorrectName == 'y':
-        continue
+        print('')
+
     else:
         inquireIfKnown()
         
 def heavyLifting(FirstName, LastName, CorrectName, createArray, UserTarget):
     #Here is where the search will start
             
-    
+    print('Starting User Search')
 
+
+print('Welcome to User Strike \n This is an information intelligence program to track and classify individuals \n You are responsible for any information you discover using this program. \n If you have any questions or suggestions please email me at tburrito@usa.com')
+ans = input('What would you like to do? \n 1. Find new user \n 2. Find multiple users \n 3.Look at found profiles \n 4. Quit \n')
+anscase(ans)
